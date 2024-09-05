48b9811b6178f27e1bef494fd3bdd681d49b4b46
Aymanbj05 <ayman.bouajaji@edu.isga.ma>
2024-09-05 19:46:39 +0100

    model of the app

--- app.py
+++ app.py
@@ -9,6 +9,34 @@ db = SQLAlchemy(app)
 
 app.secret_key = 'super secret key'
 
+class User(db.Model):
+    __tablename__ = 'users'
+    idUser = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+    idAuthor = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    books = db.relationship('Book', backref='author', lazy=True)
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    idBook = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.idAuthor'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.idCategory'), nullable=False)
+    category = db.relationship('Category', backref='books', lazy=True)
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    idCategory = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
 
 
     
