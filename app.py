0550b36e29c06b046da5af83ddd530fcfc94b3b6
Aymanbj05 <ayman.bouajaji@edu.isga.ma>
2024-09-05 19:41:42 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+app.secret_key = 'super secret key'
+
+
+
+    
+    
