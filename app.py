7c6b50b1f50e75bea8a59a37c60c62d56ced503b
Aymanbj05 <ayman.bouajaji@edu.isga.ma>
2024-09-05 19:56:16 +0100

    create db

--- app.py
+++ app.py
@@ -81,4 +81,9 @@ def register():
 
     return render_template('register.html', error=error)
     
-    
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
